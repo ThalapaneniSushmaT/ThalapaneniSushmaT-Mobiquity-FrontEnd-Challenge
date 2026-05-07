464a7ebf52b5dd30e29334800f38842e8fbae139
Sushma Thalapaneni <sushmathalapaneni@Sushmas-MacBook-Air.local>
2026-05-07 17:12:28 +0200

    full app

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  tmdbApiKey: '32d5f6911e39f6a4a8af0a02444000d6',
+  tmdbApiKey: '',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
